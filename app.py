8e36dce232686fefcb2c6915a149b79a9be533c6
dlamaton-opswerk <dj.lamaton@academy.opswerks.com>
2026-09-12 04:15:05 +0800

    changed version string

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {"VERSION": "1.0.0"}
+    return {"VERSION": "2.0.0"}
 
 if __name__ == '__main__':
     # Bind to 0.0.0.0 so it is accessible outside the container
