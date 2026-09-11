df5ec16ad43414117e8d19b5f21d12a8cb372f39
academy <academy@academys-MacBook-Pro.local>
2026-09-11 19:21:21 +0800

    Revised app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {"message": "Hello World!"}
+    return {"message": "Hello World!", "New Message": "Testing!"}
 
 if __name__ == '__main__':
     # Bind to 0.0.0.0 so it is accessible outside the container
